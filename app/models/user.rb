24bf89ad213f2d4555a010b7144c6a67c03a5828
trungkienbkhn <trungkienbkhn123@gmail.com>
2019-07-02 16:34:36 +0700

    chap 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,30 @@
 class User < ApplicationRecord
   before_save :downcase_email
-  validates :name, presence: true, length: {maximum: Settings.maximum_length_name}
+  validates :name, presence: true,
+                   length: {maximum: Settings.maximum_length_name}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: Settings.maximum_length_email},
+  validates :email, presence: true,
+                    length: {maximum: Settings.maximum_length_email},
                     format: {with: VALID_EMAIL_REGEX},
                     uniqueness: {case_sensitive: false}
   has_secure_password
-  validates :password, presence: true, length: {minimum: Settings.minimum_length_pass}
+  validates :password, presence: true,
+                       length: {minimum: Settings.minimum_length_pass}
+
+  class<<self
+    def digest string
+      cost = get_cost
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def get_cost
+      if ActiveModel::SecurePassword.min_cost
+        BCrypt::Engine::MIN_COST
+      else
+        BCrypt::Engine.cost
+      end
+    end
+  end
 
   private
 
